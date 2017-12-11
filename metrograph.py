7ce3c398b18c21438a6e2050ec41c6ee7a04835c
ho9science <full17@naver.com>
2017-12-11 19:23:26 +0900

    init graph

---
+++ metrograph.py
@@ -0,0 +1,69 @@
+import csv
+import io
+import collections
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+samdasu = {}
+with io.open('seoulmetro.csv', mode='r', encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
+    next(spamreader)
+    for row in spamreader:
+        str = "\" ".join(row)
+        str = str.replace('\"','')
+        obj = str.rstrip().split(',')
+        
+        li = []
+        li.append(obj[1])
+        li.append(obj[2])
+        samdasu[obj[3]] = li
+
+G = nx.Graph()        
+od = collections.OrderedDict(sorted(samdasu.items()))
+
+tempEdge = ""
+for fr,values in od.items():
+    G.add_node(fr,station=values[0])
+    
+    if not tempEdge :        
+        pass
+    else:
+        if fr[0] == tempEdge[0]:
+            G.add_edge(tempEdge,fr,weight=2)
+        
+    tempEdge = fr
+transfer = {}
+temp_name = ""
+for k, v in od.items():
+    
+    if not temp_name :        
+        pass
+    else:
+        for k2, v2 in od.items():
+            transfer.setdefault(v2[0], set()).add(k2)
+           
+    temp_name=v[0]
+    
+transfer_list = [key for key, values in transfer.items() if len(values) > 1]
+
+transfer_station = {}
+
+
+for station in transfer_list:
+    line_list = []
+    for k, v in od.items():
+        if station == v[0]:
+            line_list.append(k)
+    transfer_station[station]=line_list
+
+for key, value in transfer_station.items():
+    temp_value = ""
+    for i in value:
+        if not temp_value :        
+            pass
+        else:
+            G.add_edge(temp_value,i,weight=4)
+        temp_value = i
+
+print(nx.shortest_path(G,source="410",target="714")) 
