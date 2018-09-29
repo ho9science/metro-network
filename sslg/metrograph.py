06ae03603c00761d6bf99a2301a30c2017bdf7d1
samdasu <full17@naver.com>
2018-09-29 12:00:31 +0900

    modified little pythonic

--- sslg/metrograph.py
+++ sslg/metrograph.py
@@ -11,9 +11,9 @@ def readSeoulMetro():
     samdasu = {}
     opendata = os.path.join(os.path.abspath("sslg"), 'data', 'seoulmetro.csv')
     with io.open(opendata, mode='r', encoding='utf-8') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
-        next(spamreader)
-        for row in spamreader:
+        datareader = csv.reader(csvfile, delimiter=' ', quotechar=',')
+        next(datareader)
+        for row in datareader:
             str = "\" ".join(row)
             str = str.replace('\"','')
             obj = str.rstrip().split(',')
@@ -21,38 +21,29 @@ def readSeoulMetro():
             li = []
             li.append(obj[1])
             li.append(obj[2])
-            samdasu[obj[3]] = li
+            samdasu[obj[3].capitalize()] = li
         
     od = collections.OrderedDict(sorted(samdasu.items()))
     return od
 
-def makeTransferList(od):
-    transfer = {}
-    temp_name = ""
-    for k, v in od.items():
-        if not temp_name :        
-            pass
-        else:
-            for fr, name_line in od.items():
-                transfer.setdefault(name_line[0], set()).add(fr)
-
-        temp_name=v[0]
-    return transfer
-
 def makeTransferNameList(od):
-    transfer = makeTransferList(od)
+    transfer = {}
+    for fr, name_line in od.items():
+        transfer.setdefault(name_line[0], set()).add(fr)
     return [key for key, values in transfer.items() if len(values) > 1]
 
 def makeTransferFrList(od):
-    transfer = makeTransferList(od)
+    transfer = {}
+    for fr, name_line in od.items():
+        transfer.setdefault(name_line[0], set()).add(fr)
     return [values for key, values in transfer.items() if len(values) > 1]
 
 def makeTransferStationSameFr(transfer_list, od):
     transfer_station = {}
     for station in transfer_list:
         line_list = []
-        for fr, v in od.items():
-            if station == v[0]:
+        for fr, name_line in od.items():
+            if station == name_line[0]:
                 line_list.append(fr)
         transfer_station[station]=line_list
     
@@ -71,21 +62,21 @@ def makeSeoulMetroGraph():
         else:
             if fr[0] == tempEdge[0]:
                 if len(fr) >= len(tempEdge):
-                    if fr == 'k210':
+                    if fr == 'K210' or fr == 'P119':
                         continue
                     else:
-                        G.add_edge(tempEdge,fr,weight=2)
+                        G.add_edge(tempEdge, fr, weight=0.9)
                 else:
-                    G.add_edge(tempBranch,fr,weight=2.1)
+                    G.add_edge(tempBranch, fr, weight=2.1)
                 
 
             if len(fr) > len(tempEdge):
                 tempBranch = tempEdge
 
         tempEdge = fr
-    
+    # for belt line line 2
+    G.add_edge('243', '201', weight=0.9)
     transfer_list = makeTransferNameList(od)
-    #transfer_fr_list = [values for key, values in transfer.items() if len(values) > 1]
 
     transfer_station = makeTransferStationSameFr(transfer_list, od)
     
@@ -94,14 +85,14 @@ def makeSeoulMetroGraph():
         for idx, fr_name in enumerate(fr_list):
             if idx < fr_list_size:
                 for i in range(idx, fr_list_size):
-                    G.add_edge(fr_list[idx], fr_list[i+1], weight=8)
+                    G.add_edge(fr_list[idx], fr_list[i+1], weight=100)
     return G
 
 if __name__ == '__main__':
     G = makeSeoulMetroGraph()
     # print(G.edges())
-    print(nx.shortest_path(G,source="410",target="220"))
-    #print(nx.dijkstra_path(G,"410","237"))
+    print(nx.shortest_path(G,source="410",target="720"))
+    # print(nx.dijkstra_path(G,"237","410"))
     #print(nx.single_source_dijkstra_path(G, "410"))
     #print([p for p in nx.all_shortest_paths(G, source="410", target="220")])
     # print(nx.shortest_path(G,source="138",target="234-4"))
